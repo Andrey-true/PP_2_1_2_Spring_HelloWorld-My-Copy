dc2c28c8594392c6deb30d810b62ede44dbe9f74
Andrey-true <andreymgn184123@gmail.com>
2024-12-05 01:18:55 +0500

    Задача решена

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        AnnotationConfigApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(helloWorld1 == helloWorld2);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(cat1 == cat2);
+
+        applicationContext.close();
     }
 }
